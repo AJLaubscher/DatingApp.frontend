122b4e68b2fa092c0abbf18ad690a55711120884
Andre Laubscher <andre@bitcube.tech>
2025-03-29 17:01:59 +0200

    add auth gaurd

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { MembersDetailComponent } from './members/members-detail/members-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_gaurds/auth.guard';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'members', component: MembersListComponent},
+    {path: 'members', component: MembersListComponent, canActivate: [authGuard]},
     {path: 'members/:id', component: MembersDetailComponent},
     {path: 'lists', component: ListsComponent},
     {path: 'messages', component: MessagesComponent},
