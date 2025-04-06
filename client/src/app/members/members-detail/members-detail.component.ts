f801cd5fadc368a958132b3d024b36a833be1dc6
Andre Laubscher <andre@bitcube.tech>
2025-04-06 12:08:36 +0200

    adding photo gallery

--- client/src/app/members/members-detail/members-detail.component.ts
+++ client/src/app/members/members-detail/members-detail.component.ts
@@ -3,10 +3,11 @@ import { MembersService } from './../../_services/members.service';
 import { Component, inject, OnInit } from '@angular/core';
 import { Member } from '../../_models/member';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-members-detail',
-  imports: [TabsModule],
+  imports: [TabsModule, GalleryModule],
   templateUrl: './members-detail.component.html',
   styleUrl: './members-detail.component.css'
 })
@@ -14,6 +15,7 @@ export class MembersDetailComponent implements OnInit{
   private memberService = inject(MembersService);
   private route = inject(ActivatedRoute);
   member?: Member;
+  images: GalleryItem[] = [];
 
 ngOnInit(): void {
   this.loadMember();
@@ -24,7 +26,11 @@ loadMember(){
 
   if(!username) return;
   this.memberService.getMember(username).subscribe({
-    next: member => this.member = member
+  next: member => {this.member = member;
+    member.photos.map(p => {
+      this.images?.push(new ImageItem({src: p.url, thumb: p.url}))
+    })
+  }
   })
 }
 
