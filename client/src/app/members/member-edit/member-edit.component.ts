942a69aca3db7e165dfb1a58e5cde7dee43cc0ff
Andre Laubscher <andre@bitcube.tech>
2025-04-06 13:27:45 +0200

    member edit template form/ update functionality

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,12 +1,15 @@
 import { GalleryItem } from 'ng-gallery';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { Member } from '../../_models/member';
 import { AccountService } from '../../_services/account.service';
 import { MembersService } from '../../_services/members.service';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-edit',
-  imports: [],
+  imports: [TabsModule, FormsModule],
   templateUrl: './member-edit.component.html',
   styleUrl: './member-edit.component.css'
 })
@@ -14,7 +17,9 @@ export class MemberEditComponent implements OnInit{
 member?: Member;
 private accountService = inject(AccountService);
 private memberService = inject(MembersService);
+private toastr = inject(ToastrService);
 
+@ViewChild('editForm') editForm?: NgForm;
 
 ngOnInit(): void {
   this.loadMember();
@@ -26,5 +31,11 @@ loadMember(){
   this.memberService.getMember(user.username).subscribe({
     next: member => this.member = member
   })
+}
+
+updateMember(){
+  console.log(this.member);
+  this.toastr.success("Profile updated succesfully")
+  this.editForm?.reset(this.member);
 }
 }
