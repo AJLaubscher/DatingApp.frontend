b8f60a5f0787106574fb75ec164cbf9ab7b1fc89
Andre Laubscher <andre@bitcube.tech>
2025-04-20 15:36:20 +0200

    end sec 11
    end sec 11

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -2,17 +2,20 @@ import { AccountService } from './../../_services/account.service';
 import { Component, inject, input, OnInit, output } from '@angular/core';
 import { Member } from '../../_models/member';
 import { DecimalPipe, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
-import { FileUploadModule, FileUploader } from 'ng2-file-upload'
+import { FileUploadModule, FileUploader } from 'ng2-file-upload';
 import { environment } from '../../../environments/environment';
+import { MembersService } from '../../_services/members.service';
+import { Photo } from '../../_models/photo';
 
 @Component({
   selector: 'app-photo-editor',
   imports: [NgIf, NgFor, NgStyle, NgClass, FileUploadModule, DecimalPipe],
   templateUrl: './photo-editor.component.html',
-  styleUrl: './photo-editor.component.css'
+  styleUrl: './photo-editor.component.css',
 })
-export class PhotoEditorComponent implements OnInit{
+export class PhotoEditorComponent implements OnInit {
   private accountService = inject(AccountService);
+  private memberService = inject(MembersService);
   member = input.required<Member>();
   uploader?: FileUploader;
   hasBaseDropZoneOver = false;
@@ -23,12 +26,42 @@ export class PhotoEditorComponent implements OnInit{
     this.initializeUploader();
   }
 
-  fileOverBase( e: any)
-  {
+  fileOverBase(e: any) {
     this.hasBaseDropZoneOver = e;
   }
 
-  initializeUploader(){
+  deletePhoto(photo: Photo) {
+    this.memberService.deletePhoto(photo).subscribe({
+      next: (_) => {
+        const updatedMember = { ...this.member() };
+        updatedMember.photos = updatedMember.photos.filter(
+          (x) => x.id !== photo.id
+        );
+        this.memberChange.emit(updatedMember);
+      },
+    });
+  }
+
+  setMainPhoto(photo: Photo) {
+    this.memberService.setMainPhoto(photo).subscribe({
+      next: (_) => {
+        const user = this.accountService.currentUser();
+        if (user) {
+          user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(user);
+        }
+        const updatedMember = { ...this.member() };
+        updatedMember.photoUrl = photo.url;
+        updatedMember.photos.forEach((p) => {
+          if (p.isMain) p.isMain = false;
+          if (p.id === photo.id) p.isMain = true;
+        });
+        this.memberChange.emit(updatedMember);
+      },
+    });
+  }
+
+  initializeUploader() {
     this.uploader = new FileUploader({
       url: this.baseUrl + 'users/add-photo',
       authToken: 'Bearer ' + this.accountService.currentUser()?.token,
@@ -40,15 +73,14 @@ export class PhotoEditorComponent implements OnInit{
     });
 
     this.uploader.onAfterAddingFile = (file) => {
-      file.withCredentials = false
-    }
+      file.withCredentials = false;
+    };
 
     this.uploader.onSuccessItem = (item, response, status, headers) => {
       const photo = JSON.parse(response);
-      const updatedmember = {...this.member()};
-      updatedmember.photos.push(photo);
-      this.memberChange.emit(updatedmember);
-    }
-
+      const updatedMember = { ...this.member() };
+      updatedMember.photos.push(photo);
+      this.memberChange.emit(updatedMember);
+    };
   }
 }
