d32d089f166af3bca32fab16c9277f2868f17921
Andre Laubscher <andre@bitcube.tech>
2025-05-01 15:25:23 +0200

    refactor messages component for Signal R

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -3,43 +3,52 @@ import { inject, Injectable, signal } from '@angular/core';
 import { HubConnection, HubConnectionBuilder, HubConnectionState} from '@microsoft/signalr';
 import { environment } from '../../environments/environment';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PresenceService {
   hubUrl = environment.hubsUrl;
-  private hubConnection?: HubConnection;
-  private toaster = inject(ToastrService);
-  onlineUsers = signal<string[]>([]);
-  
-  
-  createHubConnection(user: User){
-    this.hubConnection = new HubConnectionBuilder()
-    .withUrl(this.hubUrl + 'presence', {
-      accessTokenFactory: () => user.token
-    })
-    .withAutomaticReconnect()
-    .build();
-
-    this.hubConnection.start().catch(error => console.log(error));
-
-    this.hubConnection.on('UserIsOnline', username => {
-      this.toaster.info(username + ' has connected');
-    });
-
-    this.hubConnection.on('UserIsOnline', username => {
-      this.toaster.warning(username + ' has disconnected');
-    });
-
-    this.hubConnection.on('GetOnlineusers', usernames => {
-      this.onlineUsers.set(usernames);
-    })
-  }
-
-  stopHubConnection(){
-    if(this.hubConnection?.state == HubConnectionState.Connected){
-      this.hubConnection.stop().catch(error => console.log(error));
-    }
-  }
-}
+   private hubConnection?: HubConnection;
+   private toastr = inject(ToastrService);
+   private router = inject(Router);
+   onlineUsers = signal<string[]>([]);
+ 
+   createHubConnection(user: User) {
+     this.hubConnection = new HubConnectionBuilder()
+       .withUrl(this.hubUrl + 'presence', {
+         accessTokenFactory: () => user.token
+       })
+       .withAutomaticReconnect()
+       .build();
+ 
+     this.hubConnection.start().catch(error => console.log(error));
+ 
+     this.hubConnection.on('UserIsOnline', username => {
+       this.onlineUsers.update(users => [...users, username]);
+     });
+ 
+     this.hubConnection.on('UserIsOffline', username => {
+       this.onlineUsers.update(users => users.filter(x => x !== username));
+     });
+ 
+     this.hubConnection.on('GetOnlineUsers', usernames => {
+       this.onlineUsers.set(usernames)
+     });
+ 
+     this.hubConnection.on('NewMessageReceived', ({username, knownAs}) => {
+       this.toastr.info(knownAs + ' has sent you a new message!  Click me to see it')
+         .onTap
+         .pipe(take(1))
+         .subscribe(() => this.router.navigateByUrl('/members/' + username + '?tab=Messages'))
+     })
+   }
+ 
+   stopHubConnection() {
+     if (this.hubConnection?.state === HubConnectionState.Connected) {
+       this.hubConnection.stop().catch(error => console.log(error))
+     }
+   }
+ }
--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,12 +1,9 @@
 import {
   Component,
-  OnInit,
   ViewChild,
   inject,
   input,
-  output,
 } from '@angular/core';
-import { Message } from '../../_models/message';
 import { MessageService } from '../../_services/message.service';
 import { TimeagoModule } from 'ngx-timeago';
 import { FormsModule, NgForm } from '@angular/forms';
@@ -19,20 +16,13 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class MemberMessagesComponent {
   @ViewChild('messageForm') messageForm?: NgForm;
-  private messageService = inject(MessageService);
+  messageService = inject(MessageService);
   username = input.required<string>();
-  messages = input.required<Message[]>();
   messageContent = '';
-  updateMessages = output<Message>();
 
   sendMessage() {
-    this.messageService
-      .sendMessage(this.username(), this.messageContent)
-      .subscribe({
-        next: (message) => {
-          this.updateMessages.emit(message);
-          this.messageForm?.reset();
-        },
+    this.messageService.sendMessage(this.username(), this.messageContent).then(() => {
+      this.messageForm?.reset();
       });
   }
 }
